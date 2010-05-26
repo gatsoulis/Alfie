0f40d7afd8c7b980646075ae41389e85087a7e6d
yianni@bumblebee <i.gatsoulis@ulster.ac.uk>
2010-05-26 11:01:27 +0100

    Hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
-#include <opencv/cxcore.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+//#include <opencv/cxcore.h>
+//#include <opencv/cv.h>
+//#include <opencv/highgui.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	const string green("../data/green.jpg");
-	const string red("../data/red.jpg");
-	string current = green;
-	cvNamedWindow("display", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("display", 100, 100);
-	IplImage* img = cvLoadImage(current.c_str(), 1);
-	cvShowImage("display", img);
-	unsigned int delay = 5;
-	for (int key = cvWaitKey(delay);; key = cvWaitKey(delay)) {
-		if (key > 0)
-			cout << key << endl;
-		if (key == 32) {
-			//cout << endl << "Spacebar pressed" << endl;
-			if (current == green)
-				current = red;
-			else
-				current = green;
-			img = cvLoadImage(current.c_str(), 1);
-			cvShowImage("display", img);
-		} else if (key == 27) {
-			break;
-		}
-	}
-	cvReleaseImage(&img);
+	cout << "Hello world" << endl;
 	return 0;
 }
