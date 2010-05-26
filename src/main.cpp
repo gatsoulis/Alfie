d8218c985667bc527edd3243e9a5eb4c109b757e
yianni@bumblebee <i.gatsoulis@ulster.ac.uk>
2010-05-26 11:03:26 +0100

    Hello Yianni

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	const string green("../data/green.jpg");
-	const string red("../data/red.jpg");
-	string current = green;
-	cvNamedWindow("display", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("display", 100, 100);
-	IplImage* img = cvLoadImage(current.c_str(), 1);
-	cvShowImage("display", img);
-	unsigned int delay = 5;
-	for (int key = cvWaitKey(delay);; key = cvWaitKey(delay)) {
-		if (key > 0)
-			cout << key << endl;
-		if (key == 32) {
-			//cout << endl << "Spacebar pressed" << endl;
-			if (current == green)
-				current = red;
-			else
-				current = green;
-			img = cvLoadImage(current.c_str(), 1);
-			cvShowImage("display", img);
-		} else if (key == 27) {
-			break;
-		}
-	}
-	cvReleaseImage(&img);
+	cout << "Hello Yianni" << endl;
+
 	return 0;
 }
